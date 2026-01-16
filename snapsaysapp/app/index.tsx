f4b90c155e5ed3e8a6ebb6e1b2a332fcabf96586
sumitsublimity <Sumit@sublimitysoft.com>
2026-01-16 21:40:27 +0530

    Initial setup

---
+++ snapsaysapp/app/index.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+import "./global.css"
+
+export default function Index() {
+  return (
+    <View
+      className="bg-red-500 h-full"
+    >
+      <Text className="text-yellow-400">Edit app/index.tsx to edit this screen.</Text>
+    </View>
+  );
+}
